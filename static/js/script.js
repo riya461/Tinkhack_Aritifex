dfcdddfeaed48e7ea4e1a33d13aee1596a06b61c
Srutav Tarun <srutavtarun@gmail.com>
2023-11-12 03:20:32 +0530

    minor change

--- static/js/script.js
+++ static/js/script.js
@@ -14,4 +14,40 @@ function previewImage(input) {
     }
 }
 
-// function outputImage()
+function watchImageContentChange() {
+    const outputImage = document.getElementById('output');
+    const box3 = document.querySelector('.box3');
+    const outputDiv = document.querySelector('.outputDiv');
+
+    let currentImageContent = ''; // Store the current image content
+
+    // Function to fetch the image content and compare it
+    const checkImageContent = async () => {
+        try {
+            const response = await fetch(outputImage.src);
+            const newImageContent = await response.blob();
+
+            if (!currentImageContent) {
+                currentImageContent = newImageContent;
+            } else if (URL.createObjectURL(currentImageContent) !== URL.createObjectURL(newImageContent)) {
+                // The image content has changed, so show the elements
+                box3.style.display = 'block';
+                outputDiv.style.display = 'block';
+
+                // Update the currentImageContent for future comparisons
+                currentImageContent = newImageContent;
+            }
+        } catch (error) {
+            console.error('Error fetching image content:', error);
+        }
+    };
+
+    // Periodically check for changes (e.g., every 5 seconds)
+    const checkInterval = 5000; // 5 seconds
+    setInterval(checkImageContent, checkInterval);
+
+    // Initial check
+    checkImageContent();
+}
+
+watchImageContentChange(); // Call the function to start watching for changes
