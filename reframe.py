ce2a610363bfa90ddc78c21adc68da066caf9f4f
adithya d <adithya.d.kochu@gmail.com>
2023-11-11 18:59:18 +0530

    Made video

---
+++ reframe.py
@@ -0,0 +1,10 @@
+import os
+import moviepy.video.io.ImageSequenceClip
+image_folder='static/files/output/format_video'
+fps=6
+
+image_files = [os.path.join(image_folder,img)
+               for img in os.listdir(image_folder)
+               if img.endswith(".jpeg")]
+clip = moviepy.video.io.ImageSequenceClip.ImageSequenceClip(image_files, fps=fps)
+clip.write_videofile('static/files/output/my_video.mp4')
