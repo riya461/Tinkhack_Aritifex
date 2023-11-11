7d197f158a3bf18c3c85066081c18d835d3ce0e8
Riya Sabu <riyasabures@gmail.com>
2023-11-11 11:33:20 +0530

    added main.py

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 import os 
 from flask import Flask, render_template, request, redirect
 
-UPLOAD_FOLDER = './files/input'
+UPLOAD_FOLDER = './files/input/'
+
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/')
+def index():
